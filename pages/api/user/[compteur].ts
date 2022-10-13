d1c06223af59945fe4fa61de40ab466ce2b1145d
leenorshn <katembovictor@gmail.com>
2022-10-13 08:46:55 +0200

    okok for deleting send for json in put endpoint

--- pages/api/user/[compteur].ts
+++ pages/api/user/[compteur].ts
@@ -3,8 +3,6 @@ import User from "../../../models/userModel";
 import connectMongo from "../../../utils/connectMongo";
 import NextCors from "nextjs-cors";
 
-
-
 export default async function handlerUserUpdate(req, res) {
   await connectMongo();
 
@@ -15,35 +13,32 @@ export default async function handlerUserUpdate(req, res) {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
   if (req.method == "PUT") {
-    const {compteur}=req.query;
-        const {somme}=req.body;
+    const { compteur } = req.query;
+    const { somme } = req.body;
 
-        try {
-            console.log(compteur);
-            
-            const userFind =await User.findOne({compteurNumber:compteur});
-            console.log(userFind);
-            
-            if(!userFind){
-              const userNoteFouond = "user not found for Updating...";
-              return res.status(404).send({code:404,message:userNoteFouond});
-            }
-            console.log(userFind);
-            
-            const userUpdeted=await User.findByIdAndUpdate(
-              userFind._id,
-              {balance:userFind.balance+somme},
-              {new:true}
-            );
-            if(userUpdeted){
-              const succesMessage = "succefully user updated ..."
-              return res.status(201).json({userUpdeted, succesMessage})
-              //return []
-            };
-          } catch (error) {
-            return res.status(500).send({code:500,message:"erreur inconnue"});
-          }
+    try {
+      const userFind = await User.findOne({ compteurNumber: compteur });
+      console.log(userFind);
+
+      if (!userFind) {
+        const userNoteFouond = "user not found for Updating...";
+        return res.status(404).json({ code: 404, message: userNoteFouond });
+      }
+      console.log(userFind);
+
+      const userUpdeted = await User.findByIdAndUpdate(
+        userFind._id,
+        { balance: userFind.balance + somme },
+        { new: true }
+      );
+      if (userUpdeted) {
+        const succesMessage = "succefully user updated ...";
+        return res.status(201).json({ userUpdeted, succesMessage });
+        //return []
+      }
+    } catch (error) {
+      return res.status(500).json({ code: 500, message: "erreur inconnue" });
+    }
   } else {
   }
 }
-
